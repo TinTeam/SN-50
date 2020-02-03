05b5481fef2a0fd27c0304ad31c4877c48a3b1bb
Luiz F. A. de Prá <rawarkanis@gmail.com>
2020-02-03 11:51:30 -0300

    Add thiserror

--- tinlib/src/common/error.rs
+++ tinlib/src/common/error.rs
@@ -1,16 +1,26 @@
 //! Error implementation and manipulation.
+use std::io;
 use std::result::Result as StdResult;
+use std::string::FromUtf8Error;
+
+use thiserror::Error;
 
 use crate::common::coord::Coord;
 use crate::common::size::Size;
 
 /// Internal errors.
-#[derive(Debug, PartialEq)]
+#[derive(Error, Debug)]
 pub enum Error {
     /// Error to reprense invalid indexes.
+    #[error("invalid index {index} for lenght {lenght}")]
     InvalidIndex { index: usize, lenght: usize },
     /// Error to represent invalid coords.
+    #[error("invalid coord ({coord:?}) for size ({size:?})")]
     InvalidCoord { coord: Coord, size: Size },
+    #[error("test")]
+    Io(#[from] io::Error),
+    #[error("test2")]
+    Utf8(#[from] FromUtf8Error),
 }
 
 impl Error {
@@ -22,6 +32,38 @@ impl Error {
     /// Creates a `InvalidCoord` error.
     pub fn new_invalid_coord(coord: Coord, size: Size) -> Self {
         Self::InvalidCoord { coord, size }
+    }
+}
+
+impl PartialEq for Error {
+    fn eq(&self, other: &Self) -> bool {
+        use Error::*;
+
+        match (self, other) {
+            (
+                InvalidIndex {
+                    index: i1,
+                    lenght: l1,
+                },
+                InvalidIndex {
+                    index: i2,
+                    lenght: l2,
+                },
+            ) => i1 == i2 && l1 == l2,
+            (
+                InvalidCoord {
+                    coord: c1,
+                    size: s1,
+                },
+                InvalidCoord {
+                    coord: c2,
+                    size: s2,
+                },
+            ) => c1 == c2 && s1 == s2,
+            (Io(_), Io(_)) => true,
+            (Utf8(_), Utf8(_)) => true,
+            _ => false,
+        }
     }
 }
 
