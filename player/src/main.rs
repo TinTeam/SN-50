794d6e59a3e062c3ae0f26db4fbfd67f72176938
Luiz F. A. de Prá <rawarkanis@gmail.com>
2025-03-31 19:48:21 -0300

    feat: add initial player code

--- player/src/main.rs
+++ player/src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     array,
+    sync::Arc,
     time::{Duration, Instant},
 };
 
@@ -7,11 +8,12 @@ use anyhow::Result;
 use log::{error, info};
 use pixels::{Pixels, SurfaceTexture};
 use winit::{
+    application::ApplicationHandler,
     dpi::LogicalSize,
-    event::{ElementState, Event, KeyEvent, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
+    event::{ElementState, KeyEvent, WindowEvent},
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
     keyboard::{Key, NamedKey},
-    window::WindowBuilder,
+    window::{Window, WindowId},
 };
 
 const WINDOW_WIDTH: u32 = 640;
@@ -24,7 +26,9 @@ const COLOR_SIZE: usize = 4;
 const TARGET_FPS: f64 = 60.0;
 const TARGET_FRAME_TIME: f64 = 1.0 / TARGET_FPS;
 
-struct GamePlayer {
+struct GamePlayer<'win> {
+    pixels: Option<Pixels<'win>>,
+    window: Option<Arc<Window>>,
     buffer: [u8; BUFFER_SIZE],
     last_color: [u8; 4],
     should_exit: bool,
@@ -33,10 +37,12 @@ struct GamePlayer {
     current_instant: Instant,
 }
 
-impl GamePlayer {
+impl GamePlayer<'_> {
     fn new() -> Self {
         let color = [0x00, 0x00, 0x00, 0xFF];
         Self {
+            pixels: None,
+            window: None,
             buffer: array::from_fn(|i| color[i % color.len()]),
             last_color: color,
             should_exit: false,
@@ -66,97 +72,83 @@ impl GamePlayer {
         }
     }
 
-    fn draw(&self, frame: &mut [u8]) {
+    fn draw(&mut self) {
+        let frame = self.pixels.as_mut().unwrap().frame_mut();
         for (i, pixel) in frame.chunks_exact_mut(COLOR_SIZE).enumerate() {
             let color = &self.buffer[i * COLOR_SIZE..(i + 1) * COLOR_SIZE];
             pixel.copy_from_slice(color);
         }
     }
-}
 
-fn handle_input(player: &mut GamePlayer, event: &KeyEvent) {
-    if event.state == ElementState::Pressed && !event.repeat {
-        match event.logical_key {
-            Key::Named(NamedKey::Escape) => player.should_exit = true,
-            Key::Named(NamedKey::Space) => player.is_paused = !player.is_paused,
-            _ => {}
+    fn handle_input(&mut self, event: &KeyEvent) {
+        if event.state == ElementState::Pressed && !event.repeat {
+            match event.logical_key {
+                Key::Named(NamedKey::Escape) => self.should_exit = true,
+                Key::Named(NamedKey::Space) => self.is_paused = !self.is_paused,
+                _ => {}
+            }
+        }
+    }
+
+    fn handle_update(&mut self) {
+        if !self.is_paused {
+            self.update();
+        }
+    }
+
+    fn handle_drawing(&mut self) {
+        self.draw();
+        if let Err(err) = self.pixels.as_ref().unwrap().render() {
+            error!("{err:?}");
+            self.should_exit = true;
         }
     }
 }
 
-fn handle_update(player: &mut GamePlayer) {
-    if !player.is_paused {
-        player.update();
-    }
-}
-
-fn handle_drawing(player: &mut GamePlayer, pixels: &mut Pixels) {
-    player.draw(pixels.frame_mut());
-    if let Err(err) = pixels.render() {
-        error!("{err:?}");
-        player.should_exit = true;
-    }
-}
-
-fn main() -> Result<()> {
-    env_logger::init();
-
-    let event_loop = EventLoop::new().unwrap();
-    let window = {
+impl ApplicationHandler for GamePlayer<'_> {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         let size = LogicalSize::new(WINDOW_WIDTH as f64, WINDOW_HEIGHT as f64);
-        WindowBuilder::new()
+        let window_attributes = Window::default_attributes()
             .with_title("SN-50 Player")
             .with_inner_size(size)
-            .with_resizable(false)
-            .build(&event_loop)
-            .unwrap()
-    };
+            .with_resizable(false);
+        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
 
-    let mut pixels = {
         let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WINDOW_WIDTH, WINDOW_HEIGHT, surface_texture)?
-    };
+        let surface_texture =
+            SurfaceTexture::new(window_size.width, window_size.height, window.clone());
+        let pixels = Pixels::new(WINDOW_WIDTH, WINDOW_HEIGHT, surface_texture).unwrap();
 
-    let mut player = GamePlayer::new();
+        self.pixels = Some(pixels);
+        self.window = Some(window);
+    }
 
-    event_loop.set_control_flow(ControlFlow::Poll);
-    event_loop.run(move |event, elwt| {
-        if player.should_exit {
-            elwt.exit();
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        if self.should_exit {
+            event_loop.exit();
             return;
         }
 
         match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                player.should_exit = true;
+            WindowEvent::CloseRequested => {
+                self.should_exit = true;
             }
-            Event::WindowEvent {
-                event:
-                    WindowEvent::KeyboardInput {
-                        event: key_event, ..
-                    },
-                ..
+            WindowEvent::KeyboardInput {
+                event: key_event, ..
             } => {
-                handle_input(&mut player, &key_event);
+                self.handle_input(&key_event);
             }
-            Event::WindowEvent {
-                event: WindowEvent::RedrawRequested,
-                ..
-            } => {
-                player.current_instant = Instant::now();
-                let elapsed = player
+            WindowEvent::RedrawRequested => {
+                self.current_instant = Instant::now();
+                let elapsed = self
                     .current_instant
-                    .duration_since(player.previous_instant)
+                    .duration_since(self.previous_instant)
                     .as_secs_f64();
-                player.previous_instant = player.current_instant;
+                self.previous_instant = self.current_instant;
                 info!("Elapsed: {}", elapsed);
 
-                handle_update(&mut player);
-                handle_drawing(&mut player, &mut pixels);
+                self.handle_update();
+                self.handle_drawing();
 
                 let delay = TARGET_FRAME_TIME - elapsed;
                 info!("Delay: {}", delay);
@@ -166,11 +158,21 @@ fn main() -> Result<()> {
 
                 info!("FPS: {}", 1.0 / (elapsed + delay));
 
-                window.request_redraw();
+                self.window.as_ref().unwrap().request_redraw();
             }
             _ => (),
         }
-    })?;
+    }
+}
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut player = GamePlayer::new();
+    event_loop.run_app(&mut player)?;
 
     Ok(())
 }
